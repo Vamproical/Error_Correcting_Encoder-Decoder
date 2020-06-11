d299ca632f71937b9a2bdd2d6290380d6768501b
Vamproical <mimoun136@yandex.ru>
2020-06-11 14:08:51 +0900

    Complete stage 1.

--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -1,7 +1,21 @@
 package correcter;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        char[] input = scanner.nextLine().toCharArray();
+
+        for (int i = 0; i < input.length; i += 3) {
+            int range = random.nextInt(Math.min(3, input.length - i));
+            char rchar = (char) (random.nextInt(96) + 32);
+
+            input[i + range] = rchar;
+        }
+
+        System.out.println(input);
     }
 }
